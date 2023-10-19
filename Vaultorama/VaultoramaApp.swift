9711e5df920014af52f4c9893f20765d6b5fff20
leojen23 <leojen23@hotmail.com>
2023-10-19 07:39:56 +0200

    First Commit

---
+++ Vaultorama/VaultoramaApp.swift
@@ -0,0 +1,17 @@
+//
+//  VaultoramaApp.swift
+//  Vaultorama
+//
+//  Created by Olivier Guillemot on 19/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct VaultoramaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
